65eba5717bded45b76ef64dc3eda3daea67c6b6f
Park jaemin <hungryshark4292@gmail.com>
2024-09-18 23:40:14 +0900

    router

---
+++ ROUTER/start/routes/user.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, APIRouter
+from pydantic import BaseModel
+
+router = APIRouter(prefix="/user", tags=["user"])
+
+
+
+@router.get("/")
+async def read_users():
+    return [{"username": "Rickie"}, {"username": "Martin"}]
+
+
+@router.get("/me")
+async def read_user_me():
+    return {"username": "currentuser"}
+
+
+@router.get("/{username}")
+async def read_user(username: str):
+    return {"username": username}
